6bf6ad53270ac41a205306d7dc81afd9bde4f7c0
Tom Deakin <tom.deakin@bristol.ac.uk>
2021-07-19 10:50:12 +0100

    Add simple OpenMP code to test build

--- src/omp/dot.cpp
+++ src/omp/dot.cpp
@@ -3,10 +3,19 @@
 
 #include <iostream>
 
+#include <omp.h>
 
 #include "../dot.hpp"
 
 void dot::run() {
   std::cout << "Hello from OpenMP dot" << std::endl;
+  int nthreads;
+
+#pragma omp parallel
+{
+#pragma single
+  nthreads = omp_get_num_threads();
+}
+std::cout << "I had " << nthreads << " threads" << std::endl;
 }
 
