bb451d8d3f94bbaa29e34b79304edbb2c9c904b6
Jason Sewall <jason.sewall@intel.com>
2021-08-17 05:49:16 -0700

    Add omp-target support
    Based off of 'plain' OpenMP model.

---
+++ src/omp-target/dot.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) 2021 Everything's Reduced authors
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+
+#include <omp.h>
+
+#include "../dot.hpp"
+#include "util.hpp"
+
+struct dot::data {
+  double *A;
+  double *B;
+};
+
+dot::dot(long N_) : N(N_), pdata{std::make_unique<data>()} {
+
+  if(!is_offloading()) {
+    std::cerr << "OMP target code is not offloading as expecting" << std::endl;
+    exit(1);
+  }
+}
+
+dot::~dot() = default;
+
+void dot::setup() {
+  pdata->A = new double[N];
+  pdata->B = new double[N];
+
+  double * A = pdata->A;
+  double * B = pdata->B;
+
+#pragma omp target enter data map(alloc:A[0:N],B[0:N])
+
+  #pragma omp parallel for
+  for (long i = 0; i < N; ++i) {
+    A[i] = 1.0 * 1024.0 / static_cast<double>(N);
+    B[i] = 2.0 * 1024.0 / static_cast<double>(N);
+  }
+
+#pragma omp target update to(A[0:N],B[0:N])
+
+}
+
+double dot::run() {
+
+  double *A = pdata->A;
+  double *B = pdata->B;
+
+  double sum = 0.0;
+
+#pragma omp target teams distribute parallel for reduction(+:sum)
+  for (long i = 0; i < N; ++i) {
+    sum += A[i] * B[i];
+  }
+
+  return sum;
+}
+
+void dot::teardown() {
+#pragma omp target exit data map(delete:pdata->A,pdata->B)
+
+  delete[] pdata->A;
+  delete[] pdata->B;
+}
---
+++ src/omp-target/field_summary.cpp
@@ -0,0 +1,143 @@
+// Copyright (c) 2021 Everything's Reduced authors
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+
+#include <omp.h>
+
+#include "../field_summary.hpp"
+
+#include "util.hpp"
+
+struct field_summary::data {
+  double *xvel;
+  double *yvel;
+  double *volume;
+  double *density;
+  double *energy;
+  double *pressure;
+};
+
+field_summary::field_summary() : pdata{std::make_unique<data>()} {
+
+  if(!is_offloading()) {
+    std::cerr << "OMP target code is not offloading as expecting" << std::endl;
+    exit(1);
+  }
+}
+
+field_summary::~field_summary() = default;
+
+void field_summary::setup() {
+  // Allocate arrays
+  pdata->xvel = new double[(nx+1) * (ny+1)];
+  pdata->yvel = new double[(nx+1) * (ny+1)];
+  pdata->volume = new double[nx * ny];
+  pdata->density = new double[nx * ny];
+  pdata->energy = new double[nx * ny];
+  pdata->pressure = new double[nx * ny];
+
+  const size_t DOF = nx*ny;
+  const size_t DOF_pad = (nx+1)*(ny+1);
+
+  double *xvel = pdata->xvel;
+  double *yvel = pdata->yvel;
+  double *volume = pdata->volume;
+  double *density = pdata->density;
+  double *energy = pdata->energy;
+  double *pressure = pdata->pressure;
+
+#pragma omp target enter data map(alloc:xvel[0:DOF_pad], yvel[0:DOF_pad], \
+  volume[0:DOF], density[0:DOF], energy[0:DOF], pressure[0:DOF])
+
+  // Initalise arrays
+  const double dx = 10.0/static_cast<double>(nx);
+  const double dy = 10.0/static_cast<double>(ny);
+
+  #pragma omp parallel for
+  for (long k = 0; k < ny; ++k) {
+    #pragma omp simd
+    for (long j = 0; j < nx; ++j) {
+
+      volume[j + k*nx] = dx * dy;
+      density[j + k*nx] = 0.2;
+      energy[j + k*nx] = 1.0;
+      pressure[j + k*nx] = (1.4-1.0) * density[j + k*nx] * energy[j + k*nx];
+    }
+  }
+
+  #pragma omp parallel for
+  for (long k = 0; k < ny/5; ++k) {
+    #pragma omp simd
+    for (long j = 0; j < nx/2; ++j) {
+
+      density[j + k*nx] = 1.0;
+      energy[j + k*nx] = 2.5;
+      pressure[j + k*nx] = (1.4-1.0) * density[j + k*nx] * energy[j + k*nx];
+    }
+  }
+
+  #pragma omp parallel for
+  for (long k = 0; k < ny+1; ++k) {
+    #pragma omp simd
+    for (long j = 0; j < nx+1; ++j) {
+      xvel[j + k*(nx+1)] = 0.0;
+      yvel[j + k*(nx+1)] = 0.0;
+    }
+  }
+
+#pragma omp target update to(xvel[0:DOF_pad], yvel[0:DOF_pad], \
+       volume[0:DOF], density[0:DOF], energy[0:DOF], pressure[0:DOF])
+
+}
+
+void field_summary::teardown() {
+#pragma omp target exit data map(delete:pdata->xvel, pdata->yvel, \
+  pdata->volume, pdata->density, pdata->energy, pdata->pressure)
+
+  delete[] pdata->xvel;
+  delete[] pdata->yvel;
+  delete[] pdata->volume;
+  delete[] pdata->density;
+  delete[] pdata->energy;
+  delete[] pdata->pressure;
+}
+
+field_summary::reduction_vars field_summary::run() {
+
+  double *xvel = pdata->xvel;
+  double *yvel = pdata->yvel;
+  double *volume = pdata->volume;
+  double *density = pdata->density;
+  double *energy = pdata->energy;
+  double *pressure = pdata->pressure;
+
+  // Reduction variables
+  double vol = 0.0;
+  double mass = 0.0;
+  double ie = 0.0;
+  double ke = 0.0;
+  double press = 0.0;
+
+  #pragma omp target teams distribute parallel for reduction(+:vol, mass, ie, ke, press)
+  for (long k = 0; k < ny; ++k) {
+    #pragma omp simd reduction(+:vol, mass, ie, ke, press)
+    for (long j = 0; j < nx; ++j) {
+      double vsqrd = 0.0;
+      for (long kv = k; kv <= k+1; ++kv) {
+        for (long jv = j; jv <= j+1; ++jv) {
+          vsqrd += 0.25 * (xvel[jv + kv*(nx+1)] * xvel[jv + kv*(nx+1)] + yvel[jv + kv*(nx+1)] * yvel[jv + kv*(nx+1)]);
+        }
+      }
+      double cell_volume = volume[j + k*nx];
+      double cell_mass = cell_volume * density[j + k*nx];
+      vol += cell_volume;
+      mass += cell_mass;
+      ie += cell_mass * energy[j + k*nx];
+      ke += cell_mass * 0.5 * vsqrd;
+      press += cell_volume * pressure[j + k*nx];
+    }
+  }
+
+  return {vol, mass, ie, ke, press};
+}
