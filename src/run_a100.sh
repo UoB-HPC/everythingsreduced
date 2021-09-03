9b7e80ad1dea8783409c1ea7a2e44699c2b69ac1
Tom Deakin <tom.deakin@bristol.ac.uk>
2021-09-03 17:02:33 +0100

    Add RAJA build to A100 script

--- src/run_a100.sh
+++ src/run_a100.sh
@@ -23,13 +23,21 @@ if [ ! -d RAJA-v0.14.0 ]; then
   tar xf RAJA-v0.14.0.tar.gz
 fi
 
-cmake -H. -Bbuild_raja -DMODEL=RAJA -DRAJA_SRC=RAJA-v0.14.0 -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DENABLE_CUDA=On -DRAJA_USE_COMPLEX=On -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/software/cuda/11.2 -DCUDA_ARCH=sm_80 -DUSING_CSD3=On
+cmake -H. -Bbuild_raja -DMODEL=RAJA -DRAJA_SRC=RAJA-v0.14.0 -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DENABLE_CUDA=On -DRAJA_USE_COMPLEX=On -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/software/cuda/11.2 -DCUDA_ARCH=sm_80 -DNO_COMPLEX_MIN=On -DCMAKE_CUDA_FLAGS_RELEASE="--expt-relaxed-constexpr"
+
+# On this system, we must use -I instead of -isystem=, however
+# because RAJA uses BLT which overwrites most of these options,
+# we have to manually patch the CMake output before using it
+for f in $(grep -Rl isystem build_raja); do
+  sed -i 's/-isystem=/-I/g' $f
+done
+
 cmake --build build_raja -v
 fi
 
 
 if [ -f ./build_raja/Reduced ]; then
-  for b in dot complex_sum complex_sum_soa field_summary describe; do
+  for b in dot complex_sum complex_sum_soa field_summary; do
     for i in $(seq 1 $runs); do
       ./build_raja/Reduced $b 1gib
     done
@@ -40,43 +48,43 @@ else
 fi
 
 ## Build Kokkos
-#if $build; then
-#if [ ! -d kokkos-3.4.01 ]; then
-#  wget https://github.com/kokkos/kokkos/archive/refs/tags/3.4.01.tar.gz
-#  tar xf 3.4.01.tar.gz
-#fi
-#
-#
-#cmake -H. -Bbuild_kokkos -DMODEL=Kokkos -DKOKKOS_SRC=kokkos-3.4.01 -DKokkos_ENABLE_CUDA=On -DKokkos_ENABLE_CUDA_LAMBDA=On -DKokkos_ARCH_AMPERE80=On -DUSING_CSD3=On
-#cmake --build build_kokkos
-#fi
-#
-#if [ -f ./build_kokkos/Reduced ]; then
-#
-#  for b in dot complex_sum complex_sum_soa complex_min field_summary describe; do
-#    for i in $(seq 1 $runs); do
-#      ./build_kokkos/Reduced $b 1gib
-#    done
-#  done
-#else
-#  echo "Build failed"
-#fi
+if $build; then
+if [ ! -d kokkos-3.4.01 ]; then
+  wget https://github.com/kokkos/kokkos/archive/refs/tags/3.4.01.tar.gz
+  tar xf 3.4.01.tar.gz
+fi
 
-## Build OpenMP
-#module load nvhpc-rhel8/21.7
-#if $build; then
-#cmake -H. -Bbuild_omp -DMODEL=OpenMP-target -DCMAKE_CXX_FLAGS="-mp=gpu" -DNO_COMPLEX_MIN=On -DNO_COMPLEX_SUM=On -DOMP_TARGET=NVIDIA
-#cmake --build build_omp
-#fi
-#
-#export OMP_TARGET_OFFLOAD=MANDATORY
-#if [ -f ./build_omp/Reduced ]; then
-#  for b in dot complex_sum_soa field_summary describe; do
-#    for i in $(seq 1 $runs); do
-#      ./build_omp/Reduced $b 1gib
-#    done
-#  done
-#else
-#  echo "Build failed"
-#fi
+
+cmake -H. -Bbuild_kokkos -DMODEL=Kokkos -DKOKKOS_SRC=kokkos-3.4.01 -DKokkos_ENABLE_CUDA=On -DKokkos_ENABLE_CUDA_LAMBDA=On -DKokkos_ARCH_AMPERE80=On -DUSING_CSD3=On
+cmake --build build_kokkos
+fi
+
+if [ -f ./build_kokkos/Reduced ]; then
+
+  for b in dot complex_sum complex_sum_soa complex_min field_summary describe; do
+    for i in $(seq 1 $runs); do
+      ./build_kokkos/Reduced $b 1gib
+    done
+  done
+else
+  echo "Build failed"
+fi
+
+# Build OpenMP
+module load nvhpc-rhel8/21.7
+if $build; then
+cmake -H. -Bbuild_omp -DMODEL=OpenMP-target -DCMAKE_CXX_FLAGS="-mp=gpu" -DNO_COMPLEX_MIN=On -DNO_COMPLEX_SUM=On -DOMP_TARGET=NVIDIA
+cmake --build build_omp
+fi
+
+export OMP_TARGET_OFFLOAD=MANDATORY
+if [ -f ./build_omp/Reduced ]; then
+  for b in dot complex_sum_soa field_summary describe; do
+    for i in $(seq 1 $runs); do
+      ./build_omp/Reduced $b 1gib
+    done
+  done
+else
+  echo "Build failed"
+fi
 
