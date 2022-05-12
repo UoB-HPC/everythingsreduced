58606e4a1f3efa7362ead65ca5458eab70fd6980
Tom Deakin <tom.deakin@bristol.ac.uk>
2022-05-12 12:00:29 +0100

    fix copy/paste error

--- src/dpcpp_cuda_compile.sh
+++ src/dpcpp_cuda_compile.sh
@@ -7,7 +7,7 @@ export CUDA_TOOLKIT_ROOT_DIR=/opt/nvidia/hpc_sdk/Linux_x86_64/21.11/cuda
 
 export CXXFLAGS="-fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --cuda-gpu-arch=sm_80 -Xcuda-ptxas --verbose --cuda-path=${CUDA_TOOLKIT_ROOT_DIR} -DSYCL_USE_NATIVE_FP_ATOMICS -O3 -ffp-contract=fast -funsafe-math-optimizations -ffp-model=fast -fsycl-id-queries-fit-in-int"
 
-export LDFLAGS="-fsycl -fsycl-targets=nvptx64-nvidia-cuda --cuda-path=${CUDA_TOOLKIT_ROOT_DIR} -Xsycl-target-backend --cuda-gpu-arch=sm_80 -Xsycl-target-backend -ptx -Xcuda-ptxas --verbose -fsycl-id-queries-fit-in-int"export LDFLAGS="-fsycl -fsycl-targets=nvptx64-nvidia-cuda --cuda-path=${CUDA_TOOLKIT_ROOT_DIR} -Xsycl-target-backend --cuda-gpu-arch=sm_80 -Xsycl-target-backend -ptx -Xcuda-ptxas --verbose -fsycl-id-queries-fit-in-int"
+export LDFLAGS="-fsycl -fsycl-targets=nvptx64-nvidia-cuda --cuda-path=${CUDA_TOOLKIT_ROOT_DIR} -Xsycl-target-backend --cuda-gpu-arch=sm_80 -Xsycl-target-backend -ptx -Xcuda-ptxas --verbose -fsycl-id-queries-fit-in-int"
 
 rm -rf build_sycl
 
